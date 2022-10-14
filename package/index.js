2b5ab7a3f7305bf9b62efe7517288deda135c164
almickle <105072712+almickle@users.noreply.github.com>
2022-10-14 13:22:26 -0600

    mmCIF parser

---
+++ package/index.js
@@ -0,0 +1,18 @@
+
+export default function parse_mmCIF(string) {
+
+    const ATOMS = file.split('\n').filter((line) => line.startsWith('ATOM')).map(array => array.split(' ').filter(element => element !== ''))
+    const customLabels = ['', 'id', 'atom', 'atom_type', '', 'residue', 'chain', 'entity_index', 'residue_index', '', 'x', 'y', 'z', 'occupancy', 'isotropic_temperature_factor', 'formal_charge', 'author_residue_index', 'author_residue', 'author_chain', 'author_type', '' ]    
+    const newAtoms = ATOMS.map((array) => {
+        return (
+            array.map((entry, index) => {
+                const label = customLabels[index]
+                if(label !== '')
+                return (
+                    {[label]: entry}
+                )}).filter((entry) => entry)
+        )
+    })
+
+    return newAtoms
+}
