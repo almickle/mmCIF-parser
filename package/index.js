138185bef108cf5f164bbd3a62f391bf9b062cab
almickle <105072712+almickle@users.noreply.github.com>
2022-10-14 13:24:37 -0600

    typo

--- package/index.js
+++ package/index.js
@@ -1,7 +1,7 @@
 
-export default function parse_mmCIF(string) {
+export default function parse_mmCIF(text) {
 
-    const ATOMS = file.split('\n').filter((line) => line.startsWith('ATOM')).map(array => array.split(' ').filter(element => element !== ''))
+    const ATOMS = text.split('\n').filter((line) => line.startsWith('ATOM')).map(array => array.split(' ').filter(element => element !== ''))
     const customLabels = ['', 'id', 'atom', 'atom_type', '', 'residue', 'chain', 'entity_index', 'residue_index', '', 'x', 'y', 'z', 'occupancy', 'isotropic_temperature_factor', 'formal_charge', 'author_residue_index', 'author_residue', 'author_chain', 'author_type', '' ]    
     const newAtoms = ATOMS.map((array) => {
         return (
