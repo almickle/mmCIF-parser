4ef5e4b211bec9fbd3e69566eb6c1783342ce552
almickle <105072712+almickle@users.noreply.github.com>
2022-10-14 13:46:15 -0600

    fixing

--- package/index.js
+++ package/index.js
@@ -4,13 +4,15 @@ export default function parse_mmCIF(text) {
     const ATOMS = text.split('\n').filter((line) => line.startsWith('ATOM')).map(array => array.split(' ').filter(element => element !== ''))
     const customLabels = ['', 'id', 'atom', 'atom_type', '', 'residue', 'chain', 'entity_index', 'residue_index', '', 'x', 'y', 'z', 'occupancy', 'isotropic_temperature_factor', 'formal_charge', 'author_residue_index', 'author_residue', 'author_chain', 'author_type', '' ]    
     const newAtoms = ATOMS.map((array) => {
+        let object = {}
+        array.forEach((entry, index) => {
+            const label = customLabels[index]
+            if(label !== '') {
+                object = ({...object, [label]: entry})
+            }
+        })
         return (
-            array.map((entry, index) => {
-                const label = customLabels[index]
-                if(label !== '')
-                return (
-                    {[label]: entry}
-                )}).filter((entry) => entry)
+            object
         )
     })
 
